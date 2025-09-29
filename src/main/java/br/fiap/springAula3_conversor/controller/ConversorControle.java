fac59c7e65878a92a5789f06e2739ab9bdb26f5f
Laboratório FIAP <labsfiap@fiap.com.br>
2025-09-29 20:57:56 -0300

    aula 29/09 arrumar erro no services bigDecimal

--- src/main/java/br/fiap/springAula3_conversor/controller/ConversorControle.java
+++ src/main/java/br/fiap/springAula3_conversor/controller/ConversorControle.java
@@ -6,6 +6,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.math.BigDecimal;
+
 @Controller
 public class ConversorControle {
     private final ConversorService service;
@@ -19,6 +21,41 @@ public class ConversorControle {
         double fahrenheit = service.celsiusToFahren(celsius);
         model.addAttribute("fahrenheit", fahrenheit);
 
+        return "index";
+    }
+
+    @PostMapping("/somar")
+    public String somar(@RequestParam BigDecimal x, @RequestParam BigDecimal y, Model model) {
+        BigDecimal result = service.somar(x, y);
+        model.addAttribute("result", result);
+
+        return "index";
+    }
+
+    @PostMapping("/calculadora")
+    public String calcular(@RequestParam String op, @RequestParam BigDecimal x, @RequestParam BigDecimal y, Model model) {
+        BigDecimal resultado = null;
+        boolean valid = true;
+
+        switch (op.toUpperCase()) {
+            case "SOMAR":
+                resultado = service.somar(x, y);
+                break;
+            case "SUBTRAIR":
+                resultado = service.subtrair(x, y);
+                break;
+            case "DIVIDIR":
+                if (y == 0) valid = false;
+                else resultado = service.dividir(x, y);
+                break;
+            case "MULTIPLICAR":
+                resultado = service.multiplicar(x, y);
+                break;
+            default:
+                valid = false;
+                break;
+        };
+        model.addAttribute("resultado", valid ? resultado : "Os dados fornecidos estão inválidos");
         return "index";
     }
 }
--- src/main/java/br/fiap/springAula3_conversor/service/ConversorService.java
+++ src/main/java/br/fiap/springAula3_conversor/service/ConversorService.java
@@ -2,9 +2,20 @@ package br.fiap.springAula3_conversor.service;
 
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
+
+import static com.fasterxml.jackson.datatype.jsr310.DecimalUtils.toBigDecimal;
+
 @Service
 public class ConversorService {
     public double celsiusToFahren(double c) {
         return c * 9/5 + 32;
+    }
+
+    public double somar(double x, double y) { return x + y; }
+
+    public BigDecimal calcular(String x, String y, String op) {
+        BigDecimal v1 = toBigDecimal(x);
+        BigDecimal v2 = toBigDecimal(y);
     }
 }
